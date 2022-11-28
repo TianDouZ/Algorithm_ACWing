62820432a6fbe3c1f1727040f4010be91ed9b8d1
ZXH <zhangxh_amanda@163.com>
2022-11-28 23:26:56 +0800

    Dijkstra

--- searchAndGraph/shortestPath/Dijkstra.cpp
+++ searchAndGraph/shortestPath/Dijkstra.cpp
@@ -18,7 +18,7 @@ using namespace std;
 const int N=510;
 
 int n,m;
-int g[N][N];  //邻接矩阵
+int g[N][N];  //邻接矩阵，稠密图用邻接矩阵存
 int dist[N]; //表示每个点距离第一个点的当前最短路径
 bool st[N]; //表示每个点的最短路是否已经确定
 
@@ -30,7 +30,8 @@ int dijkstra() {
         int t=-1; //表示还没取的点
         for(int j=1;j<=n;j++) {  //遍历所有点
             //如果j的最短路没确定，并且这点没取过或者当前的距离不是最短的(不是下一个点)
-            if(!st[j]&&(t==-1 || dist[t]>dist[j]))  t=j;
+            if(!st[j]&&(t==-1 || dist[t]>dist[j]))  t=j;  //找所有st=false的点中，dist最小的点
+            if(t==n) break;  //这是一步优化，可有可无
         }
         
         st[t]=true;
