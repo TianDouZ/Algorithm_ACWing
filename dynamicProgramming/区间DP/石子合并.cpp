10c94acd90e7289f291316a81bb8e892030cd556
ZXH <zhangxh_amanda@163.com>
2023-03-02 22:02:33 +0800

    石子合并

--- dynamicProgramming/区间DP/石子合并.cpp
+++ dynamicProgramming/区间DP/石子合并.cpp
@@ -46,3 +46,61 @@ int main() {
     return 0;
     
 }
+
+
+/*
+    编辑距离：多次套最短编辑距离DP
+    
+    询问次数:10^3个x10^3=10^6
+    编辑距离：O(n^3)
+    时间复杂度 O(n^8)   10^8
+
+    C++ 10^7~10^8/s
+*/
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+const int N=15,M=1010;
+
+int n,m;
+int f[N][N];
+char str[M][N];  //存放字符串，M个字符串每个字符串不超过N个字符
+
+int edit_distance(char a[],char b[]) {
+    int la=strlen(a+1),lb=strlen(b+1);  //求两个字符串的长度
+    
+    for(int i=0;i<=lb;i++) f[0][i]=i;
+    for(int i=0;i<=la;i++) f[i][0]=i;
+    
+    
+    for(int i=1;i<=la;i++) {
+        for(int j=1;j<=lb;j++) {
+            f[i][j]=min(f[i-1][j]+1,f[i][j-1]+1);
+            f[i][j]=min(f[i][j],f[i-1][j-1]+(a[i]!=b[j]));
+        }
+    }
+    
+    return f[la][lb];
+}
+
+int main() {
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n;i++) scanf("%s",str[i]+1);  //编辑距离，下标从1开始存
+    
+    while(m--) {
+        char s[N];
+        int limit;
+        scanf("%s%d",s+1,&limit);
+        
+        int res=0;
+        for(int i=0;i<n;i++) {
+            if(edit_distance(str[i],s)<=limit) res++;
+        }
+        printf("%d\n",res);
+    }
+    
+    return 0;
+}
