92fc6b3ad6d3b7a48f5a4570bceeab6c58611519
ZXH <zhangxh_amanda@163.com>
2023-04-05 22:37:20 +0800

    货仓选址

---
+++ greedy/绝对值不等式/货仓选址.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N=100010;
+
+int n;
+int a[N];
+
+int main() {
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    
+    sort(a,a+n);
+    int res=0;
+    for(int i=0;i<n;i++) res+=abs(a[i]-a[n/2]);  //a[n/2]就是中位数
+    
+    printf("%d\n",res);
+    
+    return 0;
+}
