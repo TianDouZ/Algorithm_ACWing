735da37a5d7b51f18845d6a816f5773ff2cd6ae0
ZXH <zhangxh_amanda@163.com>
2023-04-10 22:36:48 +0800

    stack&queue

---
+++ dataStructures/stack&queue.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+const int N=100010;
+
+int stk[N],tt;  //tt表示栈顶下标
+
+//****************************** 栈
+
+//插入
+stk[++tt]=x;  //
+
+//弹出
+tt--;
+
+//判断栈是否为空
+if(tt>0) not empty
+else empty
+
+//栈顶
+stk[tt];
+
+//****************************** 队列
+
+//在队尾插入元素，在队头弹出元素
+int q[N],hh,tt=-1;  //hh表示队头，tt表示队尾
+//tt初始化为-1，看个人习惯
+
+//插入
+q[++tt]=x;
+
+//弹出
+hh++;
+
+//判断队列是否为空
+if(hh<=tt) not empty
+else empty
+
+
+//取出队头元素
+q[hh]
+
