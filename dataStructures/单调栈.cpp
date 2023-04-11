0b9fc111944e8aa3a4a0d90faec4b82a6655f82c
ZXH <zhangxh_amanda@163.com>
2023-04-11 19:58:05 +0800

    单调栈

--- dataStructures/单调栈.cpp
+++ dataStructures/单调栈.cpp
@@ -10,7 +10,8 @@ int stk[N],tt;
 int main() {
     
     //ios::sync_with_stdio(false);  //这段语句可以来打消iostream的输入 输出缓存，可以节省许多时间
-    
+    //cin.tie(0);   用cout和cin加上这句就快了
+
     scanf("%d",&n);  //scanf printf比cin cout快很多
     
     for(int i=0;i<n;i++) {
